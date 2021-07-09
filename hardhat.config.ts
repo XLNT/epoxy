2fca45eebf80476ac5118080ba37bd3a9e1a0da6
Matt Condon <mattgcondon@gmail.com>
2021-07-08 21:26:46 -0400

    fix: disable optimizer until production

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,7 +18,7 @@ const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.4',
     settings: {
-      optimizer: { enabled: true, runs: 200 },
+      optimizer: { enabled: false, runs: 200 },
     },
   },
 };
