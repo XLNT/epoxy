999c10535e1fa447adffe1fd13c0007fdf5b62f9
Matt Condon <mattgcondon@gmail.com>
2021-07-08 21:53:48 -0400

    feat: solhint check

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import { task } from 'hardhat/config';
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-solhint';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
